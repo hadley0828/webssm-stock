5223f6f4130d6b835f1ccd33d76ce9498ca1bbda
151250122 <151250122@smail.nju.edu.cn>
2017-05-16 15:17:35 +0800

    主界面（测试）

---
+++ src/main/java/com/quantour/ssm/controller/DashBoardController.java
@@ -0,0 +1,33 @@
+package com.quantour.ssm.controller;
+
+/**
+ * Created by lenovo on 2017/5/16.
+ */
+
+import com.quantour.ssm.model.User;
+import com.quantour.ssm.service.StockService;
+import com.quantour.ssm.service.UserService;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+@RequestMapping("/dashboarc")
+public class DashBoardController {
+
+    private Logger log = Logger.getLogger(StockController.class);
+
+    @Resource
+    private StockService stockService;
+
+    @RequestMapping("")
+    public String showBoard(HttpServletRequest request, Model model){
+        return "dashboard";
+    }
+
+}
