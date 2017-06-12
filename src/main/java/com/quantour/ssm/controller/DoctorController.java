14feff19d6a394e7a877b7b21c5d98d802fe7313
151250122 <151250122@smail.nju.edu.cn>
2017-06-12 21:54:43 +0800

    doctor数据填充

--- src/main/java/com/quantour/ssm/controller/DoctorController.java
+++ src/main/java/com/quantour/ssm/controller/DoctorController.java
@@ -4,7 +4,9 @@ package com.quantour.ssm.controller;
  * Created by lenovo on 2017/6/6.
  */
 
+import com.quantour.ssm.dto.stockRate.*;
 import com.quantour.ssm.dto.userDTO;
+import com.quantour.ssm.service.RateService;
 import com.quantour.ssm.service.StockService;
 import com.quantour.ssm.service.UserService;
 import org.apache.log4j.Logger;
@@ -26,6 +28,8 @@ public class DoctorController {
     private StockService stockService;
     @Resource
     private UserService userService;
+    @Resource
+    private RateService rateService;
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String showDotor(@RequestParam(value = "id",required=false ) String user_id, HttpServletRequest request, Model model){
@@ -36,6 +40,24 @@ public class DoctorController {
 
             model.addAttribute("user",user);
         }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        try{
+            BasicDTO basicDTO = rateService.getOneStockBasicScore("000001","2017-06-02");
+            CapitalDTO capitalDTO = rateService.getOneStockCapitalScore("000001","2017-06-02");
+            IndustryDTO industryDTO = rateService.getOneStockIndustryScore("000001","2017-06-02");
+            MessageDTO messageDTO = rateService.getOneStockMessageScore("000001","2017-06-02");
+            TechnicalDTO technicalDTO = rateService.getOneStockTechnicalScore("000001","2017-06-02");
+            GeneralScoreDTO generalScoreDTO = rateService.getOneStockGeneralScore("000001","2017-06-02");
+
+            model.addAttribute("basic",basicDTO);
+            model.addAttribute("capital",capitalDTO);
+            model.addAttribute("industry",industryDTO);
+            model.addAttribute("message",messageDTO);
+            model.addAttribute("technical",technicalDTO);
+            model.addAttribute("generalScore",generalScoreDTO);
+        }catch(Exception e){
             e.printStackTrace();
         }
 
